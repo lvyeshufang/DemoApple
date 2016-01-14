e211c53106cc4507466b13ecb277a761c214acb1
Yepeng <lvyeshufang@126.com>
2016-01-14 20:53:41 +0800

    objSampleDemo001:Helloworld

---
+++ objSampleDemo001/objSampleDemo001/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objSampleDemo001
+//
+//  Created by dayang on 16/1/14.
+//  Copyright © 2016年 dy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
