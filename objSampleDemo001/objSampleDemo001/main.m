3a56f96d24dbbe522c42a4f8e24dc2dd050e8467
Yepeng <lvyeshufang@126.com>
2016-01-14 21:08:18 +0800

    print my own name and phone number .

--- objSampleDemo001/objSampleDemo001/main.m
+++ objSampleDemo001/objSampleDemo001/main.m
@@ -12,6 +12,12 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSString* name = [NSString stringWithString:@"Yepeng"];
+        NSString* phone= @"018600361752";
+        
+        NSLog(@"Name: %@ , Phone : %@" , name , phone ) ;
+        
     }
     return 0;
 }
